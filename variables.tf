0d893c5ce7f8713d1a83ea77daa106d1347d1e1a
ChinthamSridhar <31778120+ChinthamSridhar@users.noreply.github.com>
2025-10-15 16:10:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,3 @@
-variable "client_id"{
-    type = string
-    default= "3ce6c33c-9b2f-4df6-b67d-d0de3eea1d2d"
-}
-
-variable "tenant_id"{
-    type = string
-    default="380218d0-a3e8-4ef0-b7b0-94e32d8761d8"
-}
-
-variable "client_secret"{
-    type = string
-    default=".098Q~ZPiftGI6PqgLLq3yeY.31aHcIVdcmWWc8f"
-}
-
-variable "subscription_id"{
-    type = string
-    default="96af31bb-800c-451a-9c54-c314d4aeaedc"
-}
 
 variable "resource_group_name"{
     type = string
@@ -26,11 +7,6 @@ variable "resource_group_name"{
 variable "location"{
     type = string
     default = "East US"
-}
-
-variable "saname"{
-    type = string
-    default = "sapractice1dev"
 }
 
 variable "name"{
