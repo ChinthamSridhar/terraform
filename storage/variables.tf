3c40b3d1fe6f2fa9917d7c5a2c5daab9b0e7ae35
ChinthamSridhar <31778120+ChinthamSridhar@users.noreply.github.com>
2025-10-15 10:32:32 +0530

    Create variables.tf

---
+++ storage/variables.tf
@@ -0,0 +1,31 @@
+
+variable "client_id"{
+    type = string
+}
+
+variable "tenant_id"{
+    type = string
+}
+
+variable "client_secret"{
+    type = string
+}
+
+variable "subscription_id"{
+    type = string
+}
+
+variable "resource_group_name"{
+    type = string
+    default = "rg-practice-dev"
+}
+
+variable "location"{
+    type = string
+    default = "East US"
+}
+
+variable "saname"{
+    type = string
+    default = "sapractice1dev"
+}
