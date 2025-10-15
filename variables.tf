e9260bc5495436afa1ee283dfbe6c403d956c7e8
ChinthamSridhar <31778120+ChinthamSridhar@users.noreply.github.com>
2025-10-15 15:31:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,21 @@
 variable "client_id"{
     type = string
+    default= "3ce6c33c-9b2f-4df6-b67d-d0de3eea1d2d"
 }
 
 variable "tenant_id"{
     type = string
+    default="380218d0-a3e8-4ef0-b7b0-94e32d8761d8"
 }
 
 variable "client_secret"{
     type = string
+    default=".098Q~ZPiftGI6PqgLLq3yeY.31aHcIVdcmWWc8f"
 }
 
 variable "subscription_id"{
     type = string
+    default="96af31bb-800c-451a-9c54-c314d4aeaedc"
 }
 
 variable "resource_group_name"{
