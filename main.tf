94ff4971931a51b8fb3c43edaf663f9a052133a5
ChinthamSridhar <31778120+ChinthamSridhar@users.noreply.github.com>
2025-10-15 11:09:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 module "storage" {
   source                   = "./storage"
   name                     = var.saname
+  client_id                = var.client_id
+  client_secret            = var.client_secret
+  tenant_id                = var.tenant_id
+  subscription_id          = var.subscription_id
   resource_group_name      = var.resource_group_name
   location                 = var.location
 }
