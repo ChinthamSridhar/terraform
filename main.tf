eee238f85deeb5621267553642891d557e001fbf
Sridhar <chintham.sridhar@providence.org>
2025-10-16 11:50:35 +0530

    name removed

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ module "storage" {
 
 module "vnet" {
   source                   = "./vnet"
-  name                     = var.name
   resource_group_name      = var.resource_group_name
   location                 = var.location
 }
