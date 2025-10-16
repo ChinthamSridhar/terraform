46ad38583f1dab101abfd19d662ddc4f8bb1bdc8
Sridhar <chintham.sridhar@providence.org>
2025-10-16 11:43:24 +0530

    subnet name changed

---
+++ vnet/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_virtual_network" "practicevnet" {
+  name                      = var.vnetname
+  address_space             = ["10.10.0.0/16"]
+  location                  = var.location
+  resource_group_name       = var.resource_group_name
+
+    tags = {
+    environment = dev
+  }
+}
+
+resource "azurerm_subnet" "practicesubnet" {
+  name                 = var.subnetname
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.medpearlvnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
