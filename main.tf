7436affb92f4bcf8e4973d741a2455e1149eb683
Sridhar <chintham.sridhar@providence.org>
2025-10-16 11:42:17 +0530

    added vnet

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 module "storage" {
   source                   = "./storage"
+  name                     = var.saname
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+}
+
+module "vnet" {
+  source                   = "./vnet"
   name                     = var.name
   resource_group_name      = var.resource_group_name
   location                 = var.location
---
+++ vnet/variables.tf
@@ -0,0 +1,19 @@
+variable "vnetname"{
+    type = string
+    default = "vnet-practice-dev"
+}
+
+variable "resource_group_name"{
+    type = string
+    default = "rg-practice-dev"
+}
+
+variable "location"{
+    type = string
+    default = "East US"
+}
+
+variable "subnetname"{
+    type = string
+    default = "subnet-practice-dev"
+}
