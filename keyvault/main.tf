242635a3849dd24a4b7865b2a4a9fdfa71f1a07c
Sridhar <chintham.sridhar@providence.org>
2025-10-17 10:30:43 +0530

    sc added

--- keyvault/main.tf
+++ keyvault/main.tf
@@ -18,6 +18,30 @@ resource "azurerm_key_vault" "keyvault" {
   # 🔐 Network Rules
   network_acls {
     default_action             = "Deny"                      # Deny by default
-    bypass                     = "AzureServices"             # Optional: Allow trusted services
+    bypass                     = "None"             # Optional: Allow trusted services
 }
+  tags = {
+    environment = "dev"
+  }
 }
+
+data "azurerm_subnet" "kvsubnet" {
+  name                 = "kvsubnet-practice-dev"
+  virtual_network_name = "vnet-practice-dev"
+  resource_group_name  = var.resource_group_name
+}
+
+resource "azurerm_private_endpoint" "kv_pe" {
+  name                = "kv-private-endpoint"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = data.azurerm_subnet.kvsubnet.id
+
+  private_service_connection {
+    name                           = "kv-privatesc"
+    private_connection_resource_id = azurerm_key_vault.keyvault.id
+    subresource_names              = ["vault"]
+    is_manual_connection           = false
+  }
+}
+
