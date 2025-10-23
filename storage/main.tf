c0be435423a803db4ce836222cba26de7b648733
Sridhar <chintham.sridhar@providence.org>
2025-10-23 10:45:27 +0530

    added pe for sa

--- storage/main.tf
+++ storage/main.tf
@@ -15,3 +15,40 @@ resource "azurerm_storage_container" "container" {
   storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
+
+data "azurerm_subnet" "sasubnet" {
+  name                 = "sasubnet-practice-dev"
+  virtual_network_name = "vnet-practice-dev"
+  resource_group_name  = var.resource_group_name
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                 = "vnet-practice-dev"
+  resource_group_name  = var.resource_group_name
+}
+
+resource "azurerm_private_endpoint" "sa_pe" {
+  name                = "sa-private-endpoint"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = data.azurerm_subnet.sasubnet.id
+
+  private_service_connection {
+    name                           = "kv-privatesc"
+    private_connection_resource_id = azurerm_storage_account.storage.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = false
+  }
+}
+resource "azurerm_private_dns_zone" "sa_dns" {
+  name                = "privatelink.vaultcore.azure.net"
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
+  name                  = "sa-dns-vnet-link"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.sa_dns.name
+  virtual_network_id    = data.azurerm_virtual_network.vnet.id
+  registration_enabled  = false
+}
