e813ee2f17750a3b8e29c4b8fd25cafac95a7a8c
Sridhar <chintham.sridhar@providence.org>
2025-10-24 10:55:10 +0530

    adde pe acr

--- acr/main.tf
+++ acr/main.tf
@@ -6,3 +6,43 @@ resource "azurerm_container_registry" "acrpracticedev" {
   admin_enabled       = false
 }
 
+data "azurerm_subnet" "acrsubnet" {
+  name                 = "acrsubnet-practice-dev"
+  virtual_network_name = "vnet-practice-dev"
+  resource_group_name  = var.resource_group_name
+}
+
+data "azurerm_virtual_network" "vnet" {
+  name                 = "vnet-practice-dev"
+  resource_group_name  = var.resource_group_name
+}
+
+# Private Endpoint for ACR
+resource "azurerm_private_endpoint" "acr_pe" {
+  name                = "acr-private-endpoint"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = data.azurerm_subnet.acrsubnet.id
+
+  private_service_connection {
+    name                           = "acr-privatesc"
+    private_connection_resource_id = azurerm_container_registry.acrpracticedev.id
+    subresource_names              = ["registry"]
+    is_manual_connection           = false
+  }
+}
+
+# Private DNS Zone for ACR
+resource "azurerm_private_dns_zone" "acr_dns" {
+  name                = "privatelink.azurecr.io"
+  resource_group_name = var.resource_group_name
+}
+
+# DNS Zone link to VNet
+resource "azurerm_private_dns_zone_virtual_network_link" "acr_vnet_link" {
+  name                  = "acr-dns-vnet-link"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.acr_dns.name
+  virtual_network_id    = data.azurerm_virtual_network.vnet.id
+  registration_enabled  = false
+}
