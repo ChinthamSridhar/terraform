3bd3de272d82e6b99cf9995cff3db235eae64b5d
Sridhar <chintham.sridhar@providence.org>
2025-10-27 11:20:19 +0530

    added vm

--- k8s/main.tf
+++ k8s/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   default_node_pool {
     name                = "systempool"
     node_count          = 1
-    vm_size             = "standard_dc16ads_cc_v5"
+    vm_size             = "Standard_B2s"
     vnet_subnet_id      = data.azurerm_subnet.akssubnet.id
     type                = "VirtualMachineScaleSets"
   }
